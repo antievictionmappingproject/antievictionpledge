616231db4b557ffd2a416b03ccff28c3c66f6dc8
Kelsey <kelseyq@gmail.com>
2014-03-11 00:48:55 -0700

    show addresses on map

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -3,7 +3,19 @@ $(function(){
 		if(e.keyCode === 13) {
 			var parameters = { address: $(this).val() };
 			$.get( 'http://' + appConfig.apiUrlBase + '/properties', parameters, function(data) {
-				$('#results').html(data);
+				//TODO: error handling
+				map.panTo([data.lat, data.lon]);
+				map.setZoom(16);
+				if (marker !== undefined) map.removeLayer(marker);
+				marker = L.marker([data.lat, data.lon]).addTo(map);
+				//todo: template
+				var evictions = "";
+				if (data.evictions !== undefined) {
+					evictions = 'Evictions: ' + data.evictions.join(', <br/>') 
+				} 
+				marker.bindPopup('<b>'+ data.addresses.join(", <br/>") + '</b></br>' + evictions).openPopup();
+				var markerGroup = L.layerGroup([marker]);
+				console.log(data);
 			});
 		};
 	});
