d6e057923f7ce63f6ad895758c9132c20833700c
Your Name <you@example.com>
2020-03-02 14:55:52 +0400

    I did file optimization.

--- public/admin_dashboard/View/ellis.js
+++ public/admin_dashboard/View/ellis.js
@@ -1,13 +1,13 @@
 $(function () {
     if ($.session.get("myLoginKey") !== 'fdb5dfb15fdb85fd5bdf845b5fdb8fd5bfd84b5dfb854fbdf8b52dfb8df45b') {
-        window.location.href = window.location.origin + "/admin_dashboard/";
+        window.location.href = window.location.origin + "/pledge/admin_dashboard/";
     }
 });
 
 $('#logout').click(function () {
     $(function () {
         $.session.set("myLoginKey", "0");
-        window.location.href = window.location.origin + "/admin_dashboard/";
+        window.location.href = window.location.origin + "/pledge/admin_dashboard/";
     });
 
 })
@@ -128,7 +128,7 @@ $(document).ready(function () {
                     content += ' <input type="checkbox" onchange=change_status(this) attrid=' + id + ' attrstatus=' + stat + ' class="custom-control-input " ' + check + '>\n';
                     content += '<span class="custom-control-indicator"></span>\n';
                     content += '</label></td>';
-                    content += '<td style="color: #fff;"> <button type="button" class="btn" style="background:#84606a" onclick=deletePledges(' + id + ')>Delete</button> ' +
+                    content += '<td style="color: #fff;"> <button type="button" class="btn" style="background:#7d180e" onclick=deletePledges(' + id + ')>Delete</button> ' +
                         '<button type="button" class="btn" onclick=updatePledges(' + id + ') style="background:#546c7d">Update</button> </td>';
                     content += '</tr>';
                     elem.append(content);
@@ -147,6 +147,7 @@ $(document).ready(function () {
     }
 
     retrievePledges();
+
     $('#cloas_mod').click(function () {
         $('#up_modal').css('display', 'none')
     })
--- public/javascripts/ellis.js
+++ public/javascripts/ellis.js
@@ -1,22 +1,22 @@
 /**
  * Created with JetBrains RubyMine.
-  User: smagee
+ User: smagee
  * Date: 3/11/14
  * Time: 2:15 PM
- * To change this template use File | Settings | File Templates
+ * To change this template use File | Settings | File Templates.
  */
 var map;
 var marker;
+// var endpoint = "displacement-server-env-rtmfzur43y.elasticbeanstalk.com";
 var endpoint = "localhost:8888";
-// var endpoint = "pledgeserver.antievictionmap.com";
 var currentPledge = 0;
 var totalPledges = 0;
 var numColumns = 3;
 var timeoutHook;
 
 var evictionTypeHash = {
-    "Owner Move In" : '<a href="//www.sftu.org/omi.html" target="_blank">Owner Move-In</a>',
-    "Ellis Act WithDrawal" : '<a href="//www.sftu.org/ellis.html" target="_blank">Ellis Act</a>',
+    "Owner Move In" : '<a href="http://www.sftu.org/omi.html" target="_blank">Owner Move-In</a>',
+    "Ellis Act WithDrawal" : '<a href="http://www.sftu.org/ellis.html" target="_blank">Ellis Act</a>',
     "Non Payment": 'Non Payment',
     "Breach": 'Breach',
     "Nuisance": 'Nuisance',
@@ -38,9 +38,9 @@ var evictionTypeHash = {
 
 $(document).ready(function() {
     map = L.map("mapper").setView([37.760, -122.435], 12);
-    L.tileLayer('//stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png', {
+    L.tileLayer('http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', {
         maxZoom: 18,
-        attribution:  'Map tiles by <a href="//stamen.com">Stamen Design</a>, <a href="//creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="//openstreetmap.org">OpenStreetMap</a> contributors, <a href="//creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+        attribution:  'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }).addTo(map);
     //initialize event handlers
     $('#find_btn').click(function() {
@@ -55,17 +55,14 @@ $(document).ready(function() {
 
     $('#pet_link').click(function(){
         submitPledge();
-        $('#petition').fadeOut(600, function () {
-            $('#feedback').fadeIn(800)
-        });
     });
 
     $('#tw_btn').click(function() {
-        window.open("https://twitter.com/intent/tweet?text=I%20took%20a%20stand%20for%20SF%20%26%20pledged%20not%20to%20rent%20or%20buy%20units%20made%20available%20by%20eviction.%20Join%20me%3A&url=//pledge.antievictionmappingproject.net/&via=antievictionmap", '1369959514879','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
+        window.open("https://twitter.com/intent/tweet?text=I%20took%20a%20stand%20for%20SF%20%26%20pledged%20not%20to%20rent%20or%20buy%20units%20made%20available%20by%20eviction.%20Join%20me%3A&url=http://pledge.antievictionmappingproject.net/&via=antievictionmap", '1369959514879','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
     });
 
     $('#fb_btn').click(function() {
-        window.open("https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.antievictionmappingproject.net%2Fpledge%2F", '1369959514870','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
+        window.open("http://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.antievictionmappingproject.net%2Fpledge%2F", '1369959514870','width=700,height=500,toolbar=0,menubar=0,location=0,status=1,scrollbars=1,resizable=1,left=0,top=0');
     });
 
     $('#searchPledgelink').click(function() {
@@ -89,6 +86,7 @@ $(document).ready(function() {
     });
 
     $("#logo").click(function(){
+        // window.open("../update.html");
         $("html, body").animate({ scrollTop:  0}, 300);
     });
 
@@ -110,7 +108,7 @@ $(document).ready(function() {
 });
 
 function loadFromQuery() {
-   function getAddressParameter() {
+    function getAddressParameter() {
         var address = window.location.search.split("?address=")[1];
         return address && decodeURIComponent(address.replace(/\+/g, ' '));
     };
@@ -125,7 +123,7 @@ function loadFromQuery() {
 
 $(window).bind('popstate', function() {
     loadFromQuery();
- });
+});
 
 $(window).resize(function(){
     var tempColumns = $('.hideMobile:visible').length > 0 ? 3 : 1;
@@ -155,8 +153,8 @@ function scrollTo(element, time) {
 function findAndZoom(onload) {
     var geocoder = new google.maps.Geocoder();
     var address = $("#address")[0].value;
-    geocoder.geocode( { 'address': address, 'componentRestrictions': {'locality': 'San Francisco, CA'}}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK && (results[0].types.indexOf("street_address") >= 0 || results[0].types.indexOf("premise") >= 0)) {
+    geocoder.geocode( { 'address': address, 'componentRestrictions':{'locality': 'San Francisco'}}, function(results, status) {
+        if (status == google.maps.GeocoderStatus.OK && results[0].types.indexOf("street_address") >= 0) {
             var pt = [results[0].geometry.location.lat() , results[0].geometry.location.lng()];
             //adjust view so big info does not overlap search bar
             var pt2   = [results[0].geometry.location.lat() +.002, results[0].geometry.location.lng()];
@@ -180,7 +178,7 @@ function findAndZoom(onload) {
 
 function fetchEllisInfo(addressQuery, addressTxt, callback) {
     $.ajax({
-        url: "//"+endpoint+"/properties?"+addressQuery,
+        url: "http://"+endpoint+"/properties?"+addressQuery,
         type: 'GET',
         success: function(result) {
             callback(result, addressTxt);
@@ -188,12 +186,12 @@ function fetchEllisInfo(addressQuery, addressTxt, callback) {
         error: function(result) {
             var popupText = '<div class="leaflet-popup-content">' +
                 '<div class="info_window">' +
-                  '<div class="info_address without_dd">' + addressTxt + '</div>' +
-                       '<div class="info_table" style="height: 6.5em">' +
-                       'No information available about this address' +
-                       '</div>' +
-                       submitMoreDataDiv(addressTxt) +
-                       '</div></div>'
+                '<div class="info_address without_dd">' + addressTxt + '</div>' +
+                '<div class="info_table" style="height: 6.5em">' +
+                'No information available about this address' +
+                '</div>' +
+                submitMoreDataDiv(addressTxt) +
+                '</div></div>'
             marker.bindPopup(popupText, {maxWidth:500}).openPopup();
         }
     });
@@ -204,52 +202,105 @@ function submitMoreDataDiv(addressTxt) {
 }
 
 function submitPledge() {
-    var data = {
-        firstName: $('#pt_firstname').val(),
-        lastName: $('#pt_lastname').val(),
-        email: $('#pt_email').val(),
-        reason: $('#pt_reason').val(),
-        anonymous: !($('#pt_usable').is(':checked'))
-    };
-    $.ajax({
-        url: "//"+endpoint+"/pledges",
-        data: data,
-        type: 'POST',
-        success: function(result) {
-            retrievePledges();
+    var bool=true;
+    var email_Regex = /[A-Za-z]+(\.|_|-)?([A-Za-z0-9]+(\.|_|-)?)+?([A-Za-z0-9]+(\.|_|-)?)+?[A-Za-z0-9]@[A-Za-z]+(\.|_)?([A-Za-z0-9]+(\.|_|-)?)+?\.[a-z]{2,4}$/;
+    if($('#pt_firstname').val()=='' || $('#pt_firstname').val().trim(' ').length<1){
+        $('#error_name').html('Name is required');
+        bool=false;
+    }else{
+        $('#error_name').html('');
+    }
+    if($('#pt_lastname').val()=='' ||  $('#pt_lastname').val().trim(' ').length<1){
+        $('#error_lname').html('Last Name is required');
+        bool=false;
+    }else{
+        $('#error_lname').html('');
+    }
+    if(email_Regex.test($('#pt_email').val())==false ){
+        $('#error_email').html('Invalid Email');
+        bool=false;
+    }else{
+        $('#error_email').html('');
+    }
+    if($('#pt_email').val()=='' ){
+        $('#error_email').html('Email is required');
+        bool=false;
+    }
+    if($('#pt_reason').val()=='' ||  $('#pt_reason').val().trim(' ').length<1){
+        $('#error_reason').html('The field is required');
+        bool=false;
+    }else{
+        $('#error_reason').html('');
+    }
+    if(bool){
+        var data = {
+            firstName: $('#pt_firstname').val(),
+            lastName: $('#pt_lastname').val(),
+            email: $('#pt_email').val(),
+            reason: $('#pt_reason').val(),
+            anonymous: !($('#pt_usable').is(':checked'))
+        };
+        $.ajax({
+            url: "http://"+endpoint+"/pledges",
+            data: data,
+            type: 'POST',
+            success: function(result) {
+                retrievePledges();
+            }
+        });
+        $('#petition').fadeOut(600, function () {
+            $('#feedback').fadeIn(800)
+        });
+    }
+}
+
+function getTotal(evictions){
+    let result = [];
+    for (let i = 0; i < evictions.length; i++) {
+        if (result.indexOf(evictions[i].petition + evictions[i].date) == -1) {
+            result.push(evictions[i].petition + evictions[i].date);
         }
-    });
+    }
+    return result.length;
 }
 
 function openInfoWindow(result, addressTxt) {
     var obj = result;
     var text;
+    var total;
+    total = getTotal(obj.evictions);
     if (obj.evictions && obj.evictions.length > 0) {
         var subtext = "<div class='info_table'><table>";
-        var max_units = 0;
+        var units_count = 0;
         var protected = obj.hasOwnProperty("protected_tenants") ? obj.protected_tenants : 0;
         //some building have omis, some ellises, some both
         var omi = false;
         var ellis = false;
+        console.log("object", obj);
         for (var i = 0; i < obj.evictions.length; i++) {
             var ev = obj.evictions[i];
-            if (ev.eviction_type == "ellis") {
+            if (ev.eviction_type == "Ellis Act WithDrawal") {
                 ellis = true;
-            } else if (ev.eviction_type == "omi") {
+            } else if (ev.eviction_type == "Owner Move In") {
                 omi = true;
             }
             var d = new Date(ev.date);
-            if (ev.hasOwnProperty("units")) {
-                max_units = Math.max(max_units, ev.units);
+            if (ev.hasOwnProperty("units") && ev.units !== null) { //  && ev.eviction_type == "Ellis Act WithDrawal"
+                if (! isNaN(parseInt(ev.units))) {
+                    units_count += parseInt(ev.units);
+                } else {
+                    units_count++;
+                }
             }
             subtext += "<tr><td class='ev_date'>"+ d.toLocaleDateString() + "<br />" + evictionTypeHash[ev.eviction_type] + "</td><td class='ev_landlords'>";
-            if (ev.hasOwnProperty("landlords")){
+            if (ev.eviction_type == "Ellis Act WithDrawal" && ev.hasOwnProperty("landlords")){
                 subtext += "Landlords: " + ev.landlords[0];
                 for (var j = 1; j < ev.landlords.length; j++) {
                     subtext += " &bull; " + ev.landlords[j];
                 }
-            } else if (ev.hasOwnProperty("unit")){
-                subtext += ev.unit;
+            } else if (ev.hasOwnProperty("apt") && ev.apt !== null) { // ev.eviction_type == "Owner Move In" &&
+                // console.log("ev.units", ev.units);
+                subtext += ev.apt;
             } else {
                 subtext += "Detailed unit info not available";
             }
@@ -262,18 +313,9 @@ function openInfoWindow(result, addressTxt) {
         if (obj.dirty_dozen != null) {
             text += "<div class='dirty_dozen'><p class='dd_hdr' id='dd_hdr'>A Dirty Dozen Eviction<a href='" + obj.dirty_dozen + "' id='dd_lrn' target='_blank'>Learn More</a></p></div>";
         }
-        var evUnitsNum;
-        if (omi){
-            if ( ellis) {
-                evUnitsNum = max_units;
-            } else  {
-                evUnitsNum = obj.evictions.length;
-            }
-        } else {
-            evUnitsNum = max_units;
-        }
+        var evUnitsNum = units_count;
         text += "<div class='header_nums'>" +
-                 "<div class='total_col w33'><div class='circle_num redbg'>"+ obj.evictions.length +"</div><div class='ig_text red'>Total <br/ >Evictions</div></div>";
+            "<div class='total_col w33'><div class='circle_num redbg'>"+ total +"</div><div class='ig_text red'>Total <br/ >Evictions</div></div>";
         text +=  "<div class='total_col w27'><div class='circle_num bluebg'>"+ evUnitsNum +"</div><div class='ig_text blue'>Affected<br />Units</div></div>";
         text +=  "<div class='total_col w40'><div class='circle_num lightbluebg'>"+ protected +"</div><div class='ig_text lightblue'>Senior or Disabled<br />Tenants Since 2008</div></div></div>";
         text += subtext;
@@ -288,7 +330,7 @@ function openInfoWindow(result, addressTxt) {
 
 function retrievePledges() {
     $.ajax({
-        url: "//"+endpoint+"/pledges?limit="+(10 * numColumns)+"&skip="+currentPledge,
+        url: "http://"+endpoint+"/pledges?limit="+(10 * numColumns)+"&skip="+currentPledge,
         type: 'GET',
         success: function(result) {
             var rootDiv = $('<div class="pledge_columns"></div> ');
@@ -296,15 +338,17 @@ function retrievePledges() {
             var sel = $('<div id="pledgeColumn_'+j+'" class="pledgeColumn"></div>')
             var list = sel.append('<ul/>');
             for (var i = 0; i < result.length; i++){
-                if (i > 0 && i % 10 == 0){
-                    rootDiv.append(sel);
-                    j++;
-                    sel =  $('<div id="pledgeColumn_'+j+'" class="pledgeColumn hideMobile"></div>');
-                    list = sel.append('<ul/>');
+                if(result[i]['status']!=0){
+                    if (i > 0 && i % 10 == 0){
+                        rootDiv.append(sel);
+                        j++;
+                        sel =  $('<div id="pledgeColumn_'+j+'" class="pledgeColumn hideMobile"></div>');
+                        list = sel.append('<ul/>');
+                    }
+                    var reason = result[i].reason ? result[i].reason : "";
+                    var blob = '<li class="pledger"><span class="name">'+result[i].name+'</span> <span class="reason">' + reason + '</span></li>';
+                    list.append(blob);
                 }
-                var reason = result[i].reason ? result[i].reason : "";
-                var blob = '<li class="pledger"><span class="name">'+result[i].name+'</span> <span class="reason">' + reason + '</span></li>';
-                list.append(blob);
             }
             rootDiv.append(sel);
             var o = $('#pledgeColumnWrapper').find('.pledge_columns');
@@ -313,14 +357,13 @@ function retrievePledges() {
             adjustButtons();
         }
     });
-
     $.ajax({
-        url: "//"+endpoint+"/pledges/total",
+        url: "http://"+endpoint+"/pledges/total",
         type: 'GET',
         success: function(result) {
             $('#pledge_total').text(result+" people have pledged");
-            adjustButtons();
             totalPledges = result;
+            adjustButtons();
         }
     });
 }
